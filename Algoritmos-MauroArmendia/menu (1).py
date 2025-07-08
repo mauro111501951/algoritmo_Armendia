f5ab04fc1e3d2f8559bb6bce375a6cde98484067
mauro111501951 <mauro.armendia.et32@gmail.com>
2025-07-08 16:43:30 -0300

    Add files via upload
    Signed-off-by: mauro111501951 <mauro.armendia.et32@gmail.com>

---
+++ Algoritmos-MauroArmendia/menu (1).py
@@ -0,0 +1,133 @@
+
+import Funciones_Bloque_1
+import Funciones_Bloque_2
+import Funciones_Bloque_3
+bloque = int(input("Ingrese un Bloque (1-3): "))
+if bloque == 1:
+    ejercicio = int(input("Ingrese que ejercicio desea del Bloque 1 (1-19): "))
+    if ejercicio == 1:
+        Funciones_Bloque_1.Ejercicio_1()
+    elif ejercicio == 2:
+        Funciones_Bloque_1.Ejercicio_2()
+    elif ejercicio == 3:
+        Funciones_Bloque_1.Ejercicio_3()
+    elif ejercicio == 4:
+        Funciones_Bloque_1.Ejercicio_4()
+    elif ejercicio == 5:
+        Funciones_Bloque_1.Ejercicio_5()
+    elif ejercicio == 6:
+        Funciones_Bloque_1.Ejercicio_6()
+    elif ejercicio == 7:
+        Funciones_Bloque_1.Ejercicio_7()
+    elif ejercicio == 8:
+        Funciones_Bloque_1.Ejercicio_8()
+    elif ejercicio == 9:
+        Funciones_Bloque_1.Ejercicio_9()
+    elif ejercicio == 10:
+        Funciones_Bloque_1.Ejercicio_10()
+    elif ejercicio == 11:
+        Funciones_Bloque_1.Ejercicio_11()
+    elif ejercicio == 12:
+        Funciones_Bloque_1.Ejercicio_12()
+    elif ejercicio == 13:
+        Funciones_Bloque_1.Ejercicio_13()
+    elif ejercicio == 14:
+        Funciones_Bloque_1.Ejercicio_14()
+    elif ejercicio == 15:
+        Funciones_Bloque_1.Ejercicio_15()
+    elif ejercicio == 16:
+        Funciones_Bloque_1.Ejercicio_16()
+    elif ejercicio == 17:
+        Funciones_Bloque_1.Ejercicio_17()
+    elif ejercicio == 18:
+        Funciones_Bloque_1.Ejercicio_18()
+    elif ejercicio == 19:
+        Funciones_Bloque_1.Ejercicio_19()
+    else:
+        print("Ingrese un ejercicio del 1 al 19")
+elif bloque == 2:
+    ejercicio = int(input("Ingrese que ejercicio desea del Bloque 2 (1-20): "))
+    if ejercicio == 1:
+        Funciones_Bloque_2.Ejercicio_1()
+    elif ejercicio == 2:
+        Funciones_Bloque_2.Ejercicio_2()
+    elif ejercicio == 3:
+        Funciones_Bloque_2.Ejercicio_3()
+    elif ejercicio == 4:
+        Funciones_Bloque_2.Ejercicio_4()
+    elif ejercicio == 5:
+        Funciones_Bloque_2.Ejercicio_5()
+    elif ejercicio == 6:
+        Funciones_Bloque_2.Ejercicio_6()
+    elif ejercicio == 7:
+        Funciones_Bloque_2.Ejercicio_7()
+    elif ejercicio == 8:
+        Funciones_Bloque_2.Ejercicio_8()
+    elif ejercicio == 9:
+        Funciones_Bloque_2.Ejercicio_9()
+    elif ejercicio == 10:
+        Funciones_Bloque_2.Ejercicio_10()
+    elif ejercicio == 11:
+        Funciones_Bloque_2.Ejercicio_11()
+    elif ejercicio == 12:
+        Funciones_Bloque_2.Ejercicio_12()
+    elif ejercicio == 13:
+        Funciones_Bloque_2.Ejercicio_13()
+    elif ejercicio == 14:
+        Funciones_Bloque_2.Ejercicio_14()
+    elif ejercicio == 15:
+        Funciones_Bloque_2.Ejercicio_15()
+    elif ejercicio == 16:
+        Funciones_Bloque_2.Ejercicio_16()
+    elif ejercicio == 17:
+        Funciones_Bloque_2.Ejercicio_17()
+    elif ejercicio == 18:
+        Funciones_Bloque_2.Ejercicio_18()
+    elif ejercicio == 19:
+        Funciones_Bloque_2.Ejercicio_19()
+    elif ejercicio == 20:
+        Funciones_Bloque_2.Ejercicio_20()
+    else:
+        print("Debe ingresar un ejercicio del 1 al 20")
+elif bloque == 3:
+    ejercicio = int(input("Ingrese que ejercicio desea del Bloque 3 (1-18): "))
+    if ejercicio == 1:
+        Funciones_Bloque_3.Ejercicio_1()
+    elif ejercicio == 2:
+        Funciones_Bloque_3.Ejercicio_2()
+    elif ejercicio == 3:
+        Funciones_Bloque_3.Ejercicio_3()
+    elif ejercicio == 4:
+        Funciones_Bloque_3.Ejercicio_4()
+    elif ejercicio == 5:
+        Funciones_Bloque_3.Ejercicio_5()
+    elif ejercicio == 6:
+        Funciones_Bloque_3.Ejercicio_6()
+    elif ejercicio == 7:
+        Funciones_Bloque_3.Ejercicio_7()
+    elif ejercicio == 8:
+        Funciones_Bloque_3.Ejercicio_8()
+    elif ejercicio == 9:
+        Funciones_Bloque_3.Ejercicio_9()
+    elif ejercicio == 10:
+        Funciones_Bloque_3.Ejercicio_10()
+    elif ejercicio == 11:
+        Funciones_Bloque_3.Ejercicio_11()
+    elif ejercicio == 12:
+        Funciones_Bloque_3.Ejercicio_12()
+    elif ejercicio == 13:
+        Funciones_Bloque_3.Ejercicio_13()
+    elif ejercicio == 14:
+        Funciones_Bloque_3.Ejercicio_14()
+    elif ejercicio == 15:
+        Funciones_Bloque_3.Ejercicio_15()
+    elif ejercicio == 16:
+        Funciones_Bloque_3.Ejercicio_16()
+    elif ejercicio == 17:
+        Funciones_Bloque_3.Ejercicio_17()
+    elif ejercicio == 18:
+        Funciones_Bloque_3.Ejercicio_18()
+    else:
+        print("Debe ingresar un ejercicio del 1 al 18.")
+else:
+    print("Debe ingresar un bloque del 1 al 3.")
