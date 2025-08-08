a81ddd18d032ec797e3c3e29c9742a20bc4de5b4
mauro111501951 <mauro.armendia.et32@gmail.com>
2025-08-08 15:40:50 -0300

    Delete Ejercicio2.py
    Signed-off-by: mauro111501951 <mauro.armendia.et32@gmail.com>

--- Ejercicio2.py
+++
@@ -1,7 +0,0 @@
-def ejercicio2():
-    try:
-        int(input("ingrese su edad"))
-        print("su edad es valida")
-    except TypeError:
-        print("por favor ingrese un numero valido")
-ejercicio2()
