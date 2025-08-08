7a8ab10afa009da47ae57511346226ee3e80b254
mauro111501951 <mauro.armendia.et32@gmail.com>
2025-08-08 15:41:56 -0300

    Delete ejercicio1.py
    Signed-off-by: mauro111501951 <mauro.armendia.et32@gmail.com>

--- ejercicio1.py
+++
@@ -1,9 +0,0 @@
-
-n=int(input("ingrese un numero para ser dividido"))
-n1=int(input("ingrese un numero para dividir con el"))
-
-try:
-    division=n/n1
-    print(division)
-except ZeroDivisionError:
-    print("¡No se puede dividir por cero")
