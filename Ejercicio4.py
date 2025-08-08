1b813133fb9057a0242931c7f4db6e6b00728503
mauro111501951 <mauro.armendia.et32@gmail.com>
2025-08-08 15:41:30 -0300

    Delete Ejercicio4.py
    Signed-off-by: mauro111501951 <mauro.armendia.et32@gmail.com>

--- Ejercicio4.py
+++
@@ -1,9 +0,0 @@
-def ejercicio4():
-    try:
-        n1=int(input("ingrese un numero entero para sumarlo"))
-        n2=int(input("ingrese un numero entero para sumarlo"))
-        suma=n1+n2
-        print(suma)
-    except (ValueError, TypeError):
-        print("Hola, no")
-ejercicio4()
