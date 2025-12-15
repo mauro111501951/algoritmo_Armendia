f2b0435c8276005d55a6eaea5ec9ca59b708ac79
mauro111501951 <mauro.armendia.et32@gmail.com>
2025-12-14 22:17:39 -0300

    Update codigo sql farmacity.sql
    Signed-off-by: mauro111501951 <mauro.armendia.et32@gmail.com>

--- Base de datos/codigo sql farmacity.sql
+++ Base de datos/codigo sql farmacity.sql
@@ -1,32 +1,34 @@
-drop database if exists Farmacity;
-create database Farmacity;
-use Farmacity;
+drop database if exists farmacity;
+create database farmacity;
+use farmacity;
 
-create table Medicamentos(
-id_medicamentos int auto_increment primary key,
-nombre varchar(30),
-categoria varchar(50),
-precio decimal(10,2),
-stock int
+create table medicamentos (
+    id_medicamentos int auto_increment primary key,
+    nombre varchar(30),
+    categoria varchar(50),
+    precio decimal(10,2),
+    stock int
 );
-create table Ventas(
-id_venta int auto_increment primary key,
-id_medicamento int,
-fecha date,
-cantidad int,
-foreign key (id_medicamento) references Medicamentos(id_medicamento)
-);
-insert into Medicamentos(nombre, categoria, precio, stock)
-values 
-('Ibuprofeno','Analgesico',5000.00,50),
-('Tafirol','Antibiotico',4500.00,70),
-('Buscapina','Anticolinergico',7400.00,95);
 
-insert into Ventas(id_medicamento, fecha, cantidad)
+create table ventas (
+    id_venta int auto_increment primary key,
+    id_medicamento int,
+    fecha date,
+    cantidad int,
+    foreign key (id_medicamento)references medicamentos(id_medicamentos)
+);
+
+insert into medicamentos (nombre, categoria, precio, stock)
 values
-(1,'2025-3-25',5),
-(2,'2025-5-5',7),
-(1,'2025-12-8',4);
+('ibuprofeno', 'analgesico', 5000.00, 50),
+('tafirol', 'analgesico', 4500.00, 70),
+('buscapina', 'anticolinergico', 7000.00, 95);
+
+insert into ventas (id_medicamento, fecha, cantidad)
+values
+(1, '2025-3-25', 5),
+(2, '2025-5-5', 7),
+(1, '2025-12-8', 4);
 
 
 
